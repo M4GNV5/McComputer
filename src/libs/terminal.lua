e60b5e5ddf9ee1e5a37b7b2c45229a63cf4fc25d
Jakob Löw <eragonjml@googlemail.com>
2015-12-02 16:56:53 +0100

    fix terminal size

--- src/libs/terminal.lua
+++ src/libs/terminal.lua
@@ -2,8 +2,8 @@ import("terminal.js")
 
 function terminal_init()
     --40x24
-    command("fill 0 10 0 40 34 0 air")
-    command("fill 0 10 1 40 34 1 wool 15")
+    command("fill 1 10 0 40 34 0 air")
+    command("fill 1 10 1 40 34 1 wool 15")
     command("kill " + terminal_selector)
     command("summon ArmorStand 40 34 0 {CustomName:\"terminal\",NoGravity:true}")
 end
