1410ee13cba6506937bd5a69f90d9d7a82f1035b
M4GNV5 <eragonjml@googlemail.com>
2015-12-25 19:53:31 +0100

    tables yay

--- src/kernel.lua
+++ src/kernel.lua
@@ -3,27 +3,20 @@ import("./libs/terminal.lua")
 import("./libs/keyboard.lua")
 
 terminal_init()
-terminal_static_writeln("Hello World")
+terminal_static_writeln("McComputer v0")
 
-i = 0
-repeat
-    val = random()
-    val = abs(val) % 37
+local input = {}
 
-    if val < 10 then
-        val = val + 48    --number 0-9
-    elseif val == 36 then
-        val = 32          --space
-    else
-        val = val + 55    --char a-z
-    end
-
-    terminal_write(val)
-
-    i = i + 1
-until i > 10
-
-repeat
+local char = 0
+while char ~= charCode("\n") do
     char = keyboard_get()
+    input[#input + 1] = char
+
     terminal_write(char)
-until false
+end
+
+terminal_static_writeln("")
+
+for i = 1, #input do
+    terminal_write(input[i])
+end
