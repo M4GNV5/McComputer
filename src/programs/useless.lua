cf448e8d4c811801a36cb668ab2393b4d0f76e58
M4GNV5 <eragonjml@googlemail.com>
2015-12-26 23:32:40 +0100

    adding fortune command

--- src/programs/useless.lua
+++ src/programs/useless.lua
@@ -1,8 +1,36 @@
+include("stdio")
+import("random")
+
 function no_directories()
-    terminal_static_writeln("There is only one directory you fool")
+    printf("There is only one directory you fool\n")
 end
 
 function hcf()
     command("execute " .. terminal_selector .. " ~ ~ ~ setblock ~ ~ ~ fire 0 {alt:0,south:true}")
     command("kill @e[type=ArmorStand]")
+end
+
+function fortune()
+    local fd = fopen("fortunes.txt")
+    if fd == 0 then
+        printf("Fortune file not found :[\n")
+        return 1
+    end
+
+    local fortuneCount = 9
+    local line = rand_fast()
+    if line < 0 then
+        line = -line
+    end
+    line = line % fortuneCount + 1
+
+    fsetpos(fd, line, 0)
+
+    repeat
+        local c = fgetc(fd)
+        printf("%c", c)
+    until c == charCode("\n")
+
+    fclose(fd)
+    return 0
 end
