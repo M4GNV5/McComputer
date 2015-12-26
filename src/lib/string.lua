f08f8e4ce1b73b6adb5f0a6ed18d22b66801744a
M4GNV5 <eragonjml@googlemail.com>
2015-12-26 20:57:41 +0100

    more string functions

--- src/lib/string.lua
+++ src/lib/string.lua
@@ -9,3 +9,42 @@ function strsplit(str, splitter)
 
     return split, str
 end
+
+function strcmp(str1, str2)
+    if #str1 ~= #str2 then
+        return false
+    end
+
+    for i = 1, #str1, 4 do
+        local i1, i2, i3 = i + 1, i + 2, i + 3
+        local hash1 = str1[i] * (2 ^ 24) + str1[i1] * (2 ^ 16) + str1[i2] * (2 ^ 8) + str1[i3]
+        local hash2 = str2[i] * (2 ^ 24) + str2[i1] * (2 ^ 16) + str2[i2] * (2 ^ 8) + str2[i3]
+
+        if hash1 ~= hash2 then
+            return false
+        end
+    end
+    return true
+end
+
+function strcat(str1, str2)
+    if #str1 > #str2 then
+        for i = 1, #str2 do
+            str1[#str1 + 1] = str2[i]
+        end
+        return str1
+    else
+        for i = 1, #str1 do
+            table_insert(str1, 1, str2[i])
+        end
+    end
+end
+
+function strslice(str, start, stop)
+    local result = {}
+    for i = start, stop do
+        result[#result + 1] = str[i]
+    end
+
+    return result
+end
