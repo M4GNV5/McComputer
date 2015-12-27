8e5d3975caa630c4f525f8546045296fd076259b
M4GNV5 <eragonjml@googlemail.com>
2015-12-27 03:12:26 +0100

    more shell functions

--- src/shell.lua
+++ src/shell.lua
@@ -1,11 +1,21 @@
 include("stdio")
 include("string")
+include("config")
 
 import("./generate_shell.js")
 import("./shell_launch.lua")
+import("time")
+
+local printTime = false
+local printErrorCode = false
 
 function shell()
     local lastInput = {}
+
+    printf("Loading shell config...\n")
+    local config = config_parse(str("shell.conf"))
+    printTime = config[static_strhash("time")]
+    printErrorCode = config[static_strhash("code")]
 
     repeat
         printf(">")
@@ -25,6 +35,8 @@ function shell()
             printf("%c", c)
         until c == charCode("\n")
 
+        local starttime = gametime()
+
         if #input == 1 and input[1] == charCode(".") then
             input = lastInput
         else
@@ -32,13 +44,17 @@ function shell()
         end
 
         local program, args = strsplit(input, charCode(" "))
-
         local exitcode = shell_launch(program, args)
 
         if exitcode == -1 then
             printf("Unknown command %s\n", program)
-        elseif exitcode ~= 0 then
+        elseif exitcode ~= 0 and printErrorCode then
             printf("Program exited with code %d\n", exitcode)
+        elseif printTime then
+            local diff = 0.0
+            diff = gametime() - starttime
+            diff = diff / 20
+            printf("execution took %f seconds\n", diff)
         end
     until false
 end
