0561158fbaa6b16f59b7efb2bb45c5060d6f70a2
M4GNV5 <eragonjml@googlemail.com>
2015-12-28 15:43:02 +0100

    new compile scripts adding filenames, detecting changes etc.

--- compile.js
+++ compile.js
@@ -1,7 +1,19 @@
 const MOONCRAFT = "/home/jakob/Documents/Github/MoonCraft/src/main.js";
+const RCON = "/home/jakob/Documents/Github/MoonCraft/src/output/rcon.js";
+
 var path = require("path");
+var fs = require("fs");
 var spawn = require('child_process').spawn;
+
 var stuff = require("./compile.json");
+var mtimeCache = {};
+if(fs.existsSync("./compile-cache.json"))
+	mtimeCache = require("./compile-cache.json");
+
+var options = GLOBAL.options = require("./config.json");
+var rconOutput = require(RCON);
+
+var blocks = [];
 
 String.prototype.format = function()
 {
@@ -17,7 +29,16 @@ next(0);
 function next(i)
 {
     if(i >= stuff.length)
-        return;
+    {
+		if(blocks.length > 0)
+		{
+			console.log("\n\noutputting filenames and static files\n");
+			rconOutput(blocks, []);
+		}
+
+		fs.writeFileSync("./compile-cache.json", JSON.stringify(mtimeCache, undefined, 4));
+		return;
+	}
 
     var name = stuff[i].name;
     var files = stuff[i].files;
@@ -28,12 +49,80 @@ function next(i)
     else
         nextY += 3;
 
+	var lastCompile = mtimeCache[name];
+	for(var ii = 0; ii < files.length; ii++)
+	{
+		var mtime = new Date(fs.statSync(files[ii]).mtime).getTime();
+
+		if(ii + 1 == files.length && mtime < lastCompile)
+		{
+			console.log("\n\nignoring {0}".format(name));
+			setTimeout(next.bind(undefined, i + 1), 1);
+			return;
+		}
+		else if(mtime < lastCompile)
+		{
+			continue;
+		}
+
+		break;
+	}
+
     console.log("\n\ncompiling {0}\n".format(name));
+
+	createFile(name, undefined, 1, y, 5);
 
     var args = "{0} -x 1 -y {1} -z 7 --export ./include/{2}.d.lua ./src/util.lua {3} {4}"
         .format(MOONCRAFT, y, name, files.join(" "), stuff[i].args || "").trim();
 
-    run("node", args, next.bind(undefined, i + 1));
+    run("node", args, function()
+	{
+		mtimeCache[name] = new Date().getTime();
+		setTimeout(next.bind(undefined, i + 1), 1);
+	});
+}
+
+function createFile(name, content, x, y, z)
+{
+	x = x || 1;
+	z = z || 5;
+
+	function writeChar(c)
+	{
+		c = c.toUpperCase();
+		var code = c.charCodeAt(0);
+		var left = (code >> 4) & 0xF;
+		var right = code & 0xF;
+
+		writeUInt8(left);
+		z++;
+		writeUInt8(right);
+		z--;
+	}
+	function writeUInt8(val)
+	{
+		blocks.push({x: x, y: y, z: z, tagName: "wool", data: val});
+	}
+
+	var lines = [name];
+	if(content)
+		lines = lines.concat(content.split("\n"));
+
+	var _x = x;
+	for(var i = 0; i < lines.length; i++)
+	{
+		for(var ii = 0; ii < lines[i].length; ii++)
+		{
+			writeChar(lines[i][ii]);
+			x++;
+		}
+
+		if(i + 1 == lines.length && content)
+			writeChar(String.fromCharCode(0));
+
+		z += 2;
+		x = _x;
+	}
 }
 
 function run(cmd, args, cb)
@@ -61,7 +150,10 @@ function run(cmd, args, cb)
     p.on("close", function(code)
     {
         if(code != 0)
-            process.exit(code);
+		{
+			fs.writeFileSync("./compile-cache.json", JSON.stringify(mtimeCache, undefined, 4));
+			process.exit(code);
+		}
 
         cb();
     });
